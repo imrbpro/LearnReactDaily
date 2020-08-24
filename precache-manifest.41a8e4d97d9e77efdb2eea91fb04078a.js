64dc02e4fd8b92eba77f8400170754d65346cd04
Muhammad Rehan Baig <mrb5only@gmail.com>
2020-08-24 20:45:48 +0500

    Updates

---
+++ precache-manifest.41a8e4d97d9e77efdb2eea91fb04078a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894b855a6856ca48073da804c830bb1f",
+    "url": "/LearnReactDaily/index.html"
+  },
+  {
+    "revision": "3746d78c261a9a894982",
+    "url": "/LearnReactDaily/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "547afccd2ede72046e30",
+    "url": "/LearnReactDaily/static/js/2.6c7b9374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LearnReactDaily/static/js/2.6c7b9374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3746d78c261a9a894982",
+    "url": "/LearnReactDaily/static/js/main.a5ff5a2c.chunk.js"
+  },
+  {
+    "revision": "2c3fbf29fa572e8e7616",
+    "url": "/LearnReactDaily/static/js/runtime-main.1eb73e10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LearnReactDaily/static/media/logo.5d5d9eef.svg"
+  }
+]);
